f0c0266250777dbee63dd25745d34c2b9e18a195
MohithTP <tpmohith13@gmail.com>
2025-05-19 11:15:48 +0530

    changes

--- prog8.py
+++ prog8.py
@@ -2,55 +2,48 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-class KMeansClustering:
-    def _init_(self, k=3, max_iters=100, tol=1e-4):
+class kmeans():
+    def __init__(self, iter= 1000, k = 4, tol = 1e-4):
         self.k = k
-        self.max_iters = max_iters
+        self.iterations = iter
         self.tol = tol
-
-    def fit(self, X):
-        self.X = np.array(X)
-        n_samples, _ = self.X.shape
-        random_indices = np.random.choice(n_samples, self.k, replace=False)
-        self.centroids = self.X[random_indices]
-
-        for _ in range(self.max_iters):
-            self.labels = self._assign_clusters(self.X, self.centroids)
-            new_centroids = np.array([self.X[self.labels == i].mean(axis=0) for i in range(self.k)])
-            if np.all(np.linalg.norm(self.centroids - new_centroids, axis=1) < self.tol):
-                break
-            self.centroids = new_centroids
-
-    def _assign_clusters(self, X, centroids):
-        labels = []
-        for point in X:
-            distances = [np.linalg.norm(point - centroid) for centroid in centroids]
-            labels.append(np.argmin(distances))
-        return np.array(labels)
-    
-    def plot_custom_clusters(self, dim1=2, dim2=3):
-        colors = ['blue', 'green', 'orange','purple','pink']
-        for i in range(self.k):
-            cluster_points = self.X[self.labels == i]
-            plt.scatter(cluster_points[:, dim1], cluster_points[:, dim2], label=f'Cluster {i}', color=colors[i], alpha=0.6)
         
-        # Plot centroids
-        plt.scatter(self.centroids[:, dim1], self.centroids[:, dim2], marker='X', s=50, c='red', label='Centroids')
-
-        # Labels and title
-        plt.xlabel('Petal Length (cm)')
-        plt.ylabel('Petal Width (cm)')
-        plt.title('Custom K-means Clustering on Iris Dataset')
+    def fit(self, x):
+        self.X = np.array(x)
+        n, _ = self.X.shape
+        rand = np.random.choice(n, self.k, replace=False)
+        self.centroids = self.X[rand]
+        
+        for _ in range(self.iterations):
+            self.labels = []
+            for point in self.X: 
+                dist = [np.linalg.norm(point-c) for c in self.centroids]
+                self.labels.append(np.argmin(dist))
+            self.labels = np.array(self.labels)
+            new = np.array([self.X[self.labels == i].mean(axis=0) for i in range(self.k)])
+            if np.all(np.linalg.norm(self.centroids - new, axis=1) < self.tol):
+                break
+            self.centroids = new
+        
+    def plot(self, dim1 = 2, dim2 = 3):
+        colors = ['blue', 'green', 'red', 'orange','cyan']
+        for i in range(self.k):
+            points = self.X[self.labels == i]
+            plt.scatter(points[:,dim1], points[:,dim2], label = f'Cluster{i}',color = colors[i], alpha=0.6)
+            
+        plt.scatter(self.centroids[:,dim1], self.centroids[:,dim2], marker="X", s=200,c = 'red', label = "Centroids")
+        
+        plt.xlabel("Petal length")
+        plt.ylabel("Petal width")
         plt.legend()
         plt.grid(True)
         plt.show()
+    
+    
+data = pd.read_csv("iris_csv.csv")
 
-# Load dataset
-data = pd.read_csv('iris_csv.csv') 
-x = data.iloc[:, :-1].values  # Only features
+x = data.iloc[:,:-1].values
 
-# Train model
-model = KMeansClustering(k=4)
+model = kmeans()
 model.fit(x)
-
-model.plot_custom_clusters(dim1=0, dim2=1)
+model.plot()
